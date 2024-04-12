bded19bb0514b319d1ed921a0d8fc9c392272fd1
Maria-Papa <1maria.papa@gmail.com>
2024-04-12 17:03:24 +0300

    GUI works

--- gui.py
+++ gui.py
@@ -1,25 +1,28 @@
 import tkinter as tk
+import tkinter.font as font
+from turtle import width
 from cli import start
-from variables import black
-from tkinter import LEFT, NE, NW, SE, BooleanVar, Scrollbar, StringVar, ttk, DISABLED, END, NORMAL, VERTICAL, NS, N, W, E, S, EW
+from variables import black, green
+from tkinter import LEFT, NE, NSEW, NW, SE, BooleanVar, Grid, Scrollbar, StringVar, ttk, DISABLED, END, NORMAL, VERTICAL, NS, N, W, E, S, EW
 
 class GUI(tk.Tk):
     def __init__(self):
         super().__init__()
 
         self.title("Music Track Fixer")
-        self.resizable(0, 0)
-        self._center_window(1500, 800)
-
-        self.main_frame = self._create_grid("25 25 25 25")
-        
-        # self._configure_grid()
+        self.resizable(False, False)
+        self._center_window()
+        self._create_grid("25 25 25 25")
         self._create_widgets()
 
-    def _center_window(self, ww, wh):
+    def _center_window(self, per_of_screen_w=40, per_of_screen_h=60):
         # Get the screen dimension
         sw = self.winfo_screenwidth()
         sh = self.winfo_screenheight()
+
+        # Covert the percentage of the screen to window width & height
+        ww = int(sw*(per_of_screen_w/100))
+        wh = int(sh*(per_of_screen_h/100))
 
         # Find the center point
         center_x = int(sw/2 - ww / 2)
@@ -29,17 +32,17 @@ class GUI(tk.Tk):
         self.geometry(f'{ww}x{wh}+{center_x}+{center_y}')
 
     def _create_grid(self, padding):
-        mf = ttk.Frame(self, padding=padding)
-        mf.grid(column=0, row=0, sticky=(N, W, E, S))
-        return mf
+        self.main_frame = ttk.Frame(self, padding=padding)
+        self._configure_grid()
+        self.main_frame.pack(fill='both', expand=True)
     
     def _configure_grid(self):
-        self.columnconfigure(0, weight=1)
-        self.columnconfigure(1, weight=1)
-        self.columnconfigure(2, weight=1)
-        self.rowconfigure(0, weight=1)
-        self.rowconfigure(1, weight=1)
-        self.rowconfigure(2, weight=3)
+        self.main_frame.columnconfigure(0, weight=1)
+        self.main_frame.columnconfigure(1, weight=1)
+        self.main_frame.columnconfigure(2, weight=1)
+        self.main_frame.rowconfigure(0, weight=1)
+        self.main_frame.rowconfigure(1, weight=3)
+        self.main_frame.rowconfigure(2, weight=1)
         
     def _start(self):
         start(self)
@@ -47,30 +50,33 @@ class GUI(tk.Tk):
     def _create_widgets(self):
         # PATH LABEL
         self.path_label = ttk.Label(self.main_frame, text="Music Files Location:")
-        self.path_label.grid(row=0, column=0, sticky=NW, padx=5, pady=5)
+        self.path_label.grid(row=0, column=0, sticky=W, padx=5, pady=5)
 
         # PATH ENTRY
         self.path       = StringVar()
-        self.path_entry = ttk.Entry(self.main_frame, width=100, textvariable=self.path)
-        self.path_entry.grid(row=0, column=1, sticky=NW, padx=5, pady=5)
+        self.path_entry = ttk.Entry(self.main_frame, width=150, textvariable=self.path)
+        self.path_entry.grid(row=0, column=1, sticky=EW, padx=5, pady=5)
 
         # SUBDIR CHECKBUTTON
         self.subdir              = BooleanVar()
         self.sub_dir_ckeckbutton = ttk.Checkbutton(self.main_frame, text="Include Subdirectories", name="sub_dir", variable=self.subdir, onvalue=1, offvalue=0, state="normal")
-        self.sub_dir_ckeckbutton.grid(row=0, column=2, sticky=NE, padx=5, pady=5)
+        self.sub_dir_ckeckbutton.grid(row=0, column=2, sticky=E, padx=5, pady=5)
 
         # OUTPUT TEXT
         self.output      = StringVar()
         self.output_text = tk.Text(self.main_frame, state=DISABLED)
-        self.output_text.grid(row=1, column=0, columnspan=3, rowspan=3, sticky=EW, padx=5, pady=5)
+        self.output_text.grid(row=1, column=0, columnspan=3, sticky=NSEW, padx=5, pady=5)
 
         # SCROLLBAR
         self.scroll = Scrollbar(self.main_frame, orient=VERTICAL, command=self.output_text.yview)
-        self.scroll.grid(row=1, column=0, columnspan=3, rowspan=3, sticky=(NS, E), padx=5, pady=5)
+        self.scroll.grid(row=1, column=0, columnspan=3, sticky=(NS, E), padx=5, pady=5)
 
         # START BUTTON
+        style = ttk.Style()
+        style.configure("TButton", font=("calibri", 16, "bold"), background=green, foreground=green)
         self.start_button = ttk.Button(self.main_frame, text="Start", command=self._start)
-        self.start_button.grid(row=5, column=0, columnspan=3, rowspan=2, sticky=EW, padx=5, pady=5)
+        self.start_button.grid(row=2, column=0, columnspan=3, sticky=EW, padx=5, pady=5)
+
 
     def get_subdir_value(self):
         return self.subdir.get()
