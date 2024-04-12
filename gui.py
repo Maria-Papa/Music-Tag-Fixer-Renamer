64bf112e50b857dc5a5f1809e068234f30b379f3
Maria-Papa <1maria.papa@gmail.com>
2024-04-12 21:26:20 +0300

    Fix widgets positions

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from cli import start
 from variables import black, green
-from tkinter import BooleanVar, Scrollbar, StringVar, ttk, DISABLED, END, NORMAL, VERTICAL, NSEW, NS, EW, W, E
+from tkinter import BooleanVar, Scrollbar, StringVar, ttk, DISABLED, END, NORMAL, VERTICAL, NSEW, NS, EW, E
 
 class GUI(tk.Tk):
     def __init__(self):
@@ -10,7 +10,7 @@ class GUI(tk.Tk):
         self.title("Music Track Fixer")
         self.resizable(False, False)
         self._center_window()
-        self._create_grid("25 25 25 25")
+        self._create_grid("20 20 20 20")
         self._create_widgets()
 
     def _center_window(self, per_of_screen_w=40, per_of_screen_h=60):
@@ -31,50 +31,45 @@ class GUI(tk.Tk):
 
     def _create_grid(self, padding):
         self.main_frame = ttk.Frame(self, padding=padding)
+        self.main_frame.pack(fill="both", expand=True)
         self._configure_grid()
-        self.main_frame.pack(fill='both', expand=True)
     
     def _configure_grid(self):
-        self.main_frame.columnconfigure(0, weight=1)
         self.main_frame.columnconfigure(1, weight=1)
-        self.main_frame.columnconfigure(2, weight=1)
-        self.main_frame.rowconfigure(0, weight=1)
-        self.main_frame.rowconfigure(1, weight=3)
-        self.main_frame.rowconfigure(2, weight=1)
+        self.main_frame.rowconfigure(1, weight=2)
         
     def _start(self):
         start(self)
 
     def _create_widgets(self):
-        # PATH LABEL
+        # ROW 0 - PATH LABEL
         self.path_label = ttk.Label(self.main_frame, text="Music Files Location:")
-        self.path_label.grid(row=0, column=0, sticky=W, padx=5, pady=5)
+        self.path_label.grid(row=0, column=0)
 
-        # PATH ENTRY
+        # ROW 0 - PATH ENTRY
         self.path       = StringVar()
         self.path_entry = ttk.Entry(self.main_frame, width=150, textvariable=self.path)
-        self.path_entry.grid(row=0, column=1, sticky=EW, padx=5, pady=5)
+        self.path_entry.grid(row=0, column=1, sticky=EW, padx=20)
 
-        # SUBDIR CHECKBUTTON
+        # ROW 0 - SUBDIR CHECKBUTTON
         self.subdir              = BooleanVar()
-        self.sub_dir_ckeckbutton = ttk.Checkbutton(self.main_frame, text="Include Subdirectories", name="sub_dir", variable=self.subdir, onvalue=1, offvalue=0, state="normal")
-        self.sub_dir_ckeckbutton.grid(row=0, column=2, sticky=E, padx=5, pady=5)
+        self.sub_dir_ckeckbutton = ttk.Checkbutton(self.main_frame, text="Include Subdirectories", name="sub_dir", variable=self.subdir, onvalue=1, offvalue=0, state=NORMAL)
+        self.sub_dir_ckeckbutton.grid(row=0, column=2)
 
-        # OUTPUT TEXT
+        # ROW 1 - OUTPUT TEXT
         self.output      = StringVar()
         self.output_text = tk.Text(self.main_frame, state=DISABLED)
-        self.output_text.grid(row=1, column=0, columnspan=3, sticky=NSEW, padx=5, pady=5)
+        self.output_text.grid(row=1, column=0, columnspan=3, sticky=NSEW, pady=20)
 
-        # SCROLLBAR
+        # ROW 1 - SCROLLBAR
         self.scroll = Scrollbar(self.main_frame, orient=VERTICAL, command=self.output_text.yview)
-        self.scroll.grid(row=1, column=0, columnspan=3, sticky=(NS, E), padx=5, pady=5)
+        self.scroll.grid(row=1, column=0, columnspan=3, sticky=(NS, E), pady=20)
 
-        # START BUTTON
+        # ROW 3 - START BUTTON
         style = ttk.Style()
         style.configure("TButton", font=("calibri", 16, "bold"), background=green, foreground=green)
         self.start_button = ttk.Button(self.main_frame, text="Start", command=self._start)
-        self.start_button.grid(row=2, column=0, columnspan=3, sticky=EW, padx=5, pady=5)
-
+        self.start_button.grid(row=2, column=0, columnspan=3, sticky=EW)
 
     def get_subdir_value(self):
         return self.subdir.get()
