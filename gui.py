1402d2be95f627697f1d18d46296dca6530392ab
Maria-Papa <1maria.papa@gmail.com>
2024-04-12 15:28:48 +0300

    Rethinking gui

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 import tkinter as tk
-from tkinter import SE, VERTICAL, BooleanVar, Scrollbar, StringVar, ttk, DISABLED, END, NORMAL, NS, N, W, E, S
 from cli import start
+from variables import black
+from tkinter import LEFT, NE, NW, SE, BooleanVar, Scrollbar, StringVar, ttk, DISABLED, END, NORMAL, VERTICAL, NS, N, W, E, S, EW
 
 class GUI(tk.Tk):
     def __init__(self):
@@ -8,11 +9,11 @@ class GUI(tk.Tk):
 
         self.title("Music Track Fixer")
         self.resizable(0, 0)
-        self._center_window(700, 400)
+        self._center_window(1500, 800)
 
         self.main_frame = self._create_grid("25 25 25 25")
         
-        self._configure_grid()
+        # self._configure_grid()
         self._create_widgets()
 
     def _center_window(self, ww, wh):
@@ -46,31 +47,30 @@ class GUI(tk.Tk):
     def _create_widgets(self):
         # PATH LABEL
         self.path_label = ttk.Label(self.main_frame, text="Music Files Location:")
-        self.path_label.grid(row=0, column=0, sticky=(N, W), pady=5)
+        self.path_label.grid(row=0, column=0, sticky=NW, padx=5, pady=5)
 
         # PATH ENTRY
         self.path       = StringVar()
-        self.path_entry = ttk.Entry(self.main_frame, width=80, textvariable=self.path)
-        self.path_entry.grid(row=0, column=1, columnspan= 2, sticky=(N, E), pady=5)
+        self.path_entry = ttk.Entry(self.main_frame, width=100, textvariable=self.path)
+        self.path_entry.grid(row=0, column=1, sticky=NW, padx=5, pady=5)
 
         # SUBDIR CHECKBUTTON
-        self.subdir             = BooleanVar()
+        self.subdir              = BooleanVar()
         self.sub_dir_ckeckbutton = ttk.Checkbutton(self.main_frame, text="Include Subdirectories", name="sub_dir", variable=self.subdir, onvalue=1, offvalue=0, state="normal")
-        self.sub_dir_ckeckbutton.grid(row=1, column=1, columnspan= 2, sticky=(N, W), pady=5)
-
-        # START BUTTON
-        self.start_button = ttk.Button(self.main_frame, text="Start", width=20, command=self._start)
-        self.start_button.grid(row=1, column=2, sticky=(N, E), pady=5)
+        self.sub_dir_ckeckbutton.grid(row=0, column=2, sticky=NE, padx=5, pady=5)
 
         # OUTPUT TEXT
         self.output      = StringVar()
-        self.output_text = tk.Text(self.main_frame, width=80, height=17, state=DISABLED)
-        # self.output_text = ttk.Entry(self.main_frame, width=80, state=DISABLED)
-        self.output_text.grid(row=2, column=0, columnspan=3, rowspan=3, sticky=NS, pady=5)
+        self.output_text = tk.Text(self.main_frame, state=DISABLED)
+        self.output_text.grid(row=1, column=0, columnspan=3, rowspan=3, sticky=EW, padx=5, pady=5)
 
         # SCROLLBAR
         self.scroll = Scrollbar(self.main_frame, orient=VERTICAL, command=self.output_text.yview)
-        self.scroll.grid(row=2, column=0, columnspan=3, rowspan=3, sticky=(NS, E), pady=5)
+        self.scroll.grid(row=1, column=0, columnspan=3, rowspan=3, sticky=(NS, E), padx=5, pady=5)
+
+        # START BUTTON
+        self.start_button = ttk.Button(self.main_frame, text="Start", command=self._start)
+        self.start_button.grid(row=5, column=0, columnspan=3, rowspan=2, sticky=EW, padx=5, pady=5)
 
     def get_subdir_value(self):
         return self.subdir.get()
@@ -78,7 +78,7 @@ class GUI(tk.Tk):
     def get_path_value(self):
         return self.path.get()
     
-    def set_output_value(self, output_value, fg, delete=False):
+    def set_output_value(self, output_value, fg=black, delete=False):
         self.output_text.configure(state=NORMAL, fg=fg)
 
         if delete is True:
